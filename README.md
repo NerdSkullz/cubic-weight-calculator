02c9a7c4eddb68d41542a6d75a0dc32dc6b3929a
Pietro Di Marzo <pietro@hhd.com.au>
2018-05-31 07:39:47 +1000

    added some comments and instructions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Cubic Weight Calculator
+
+# To start the project navigate to the directory
+# start a simple server running a command like ( 'python -m SimpleHTTPServer' )
+# connect to 'http://localhost:8000/' if you're running the above script
+# or connect to your localhost to see the result
+
+# SPECIAL CONSIDERATIONS
+# I haven't used any tool like webpack or node or style or anything at all
+# simply because I thought the challenge was around showing how I would call
+# the API, loop the results, filter them and manipulate their informations.
+# This is JUST a demonstration and I'm aware that in a production environment
+# there are aspects like compatibility, script size etc... to consider.
--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+// Fetching API
 const requestAirConditiong = async () => {
   let allData = [];
   let morePagesAvailable = true;
   let currentPage = 0;
-
   while(morePagesAvailable) {
     currentPage++;
     let url = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/' + currentPage;
     const response = await fetch(url);
     let data = await response.json(),
         next_page = data.next;
-
+    // Special check if there are other pages since the field next doesn't play nice and it responds with a letter
     if (next_page === null) {
       morePagesAvailable = false
     } else {
       next_page = next_page.replace("/api/products/", "")
     }
+    // Looping the array of objects to find JUST air conditioners
     data.objects.forEach(e => {
       if (e.category === "Air Conditioners") {
         allData.unshift(e);
@@ -24,6 +25,7 @@ const requestAirConditiong = async () => {
   return allData;
 }
 
+// Funciton to truncate numbers without rounding them
 const truncateNumbers = number => {
   return Math.floor((number/100) * 100) / 100;
 }
@@ -32,9 +34,11 @@ const convertCmToM = (size) => {
   return truncateNumbers(size)
 }
 
+// Caluculating average cubic weight
 const getAverageCubicWeight = (elements) => {
   let cubicWeights = [],
       reducer = (accumulator, currentValue) => accumulator + currentValue;
+  // Looping array of Air Conditioners to get sizes and add them into a new array
   elements.forEach(e => {
     let width = convertCmToM(e.size.width),
         lenght = convertCmToM(e.size.length),
@@ -42,13 +46,14 @@ const getAverageCubicWeight = (elements) => {
         cubicWeight = (lenght * height * width) * 250;
     cubicWeights.unshift(cubicWeight)
   })
-  console.log(cubicWeights.reduce(reducer)/cubicWeights.length)
+  // Reduce array adding single cubic weight and dividing the result by the lenght of the array
   document.querySelector('#average_label').innerHTML = cubicWeights.reduce(reducer)/cubicWeights.length;
 }
 
 (function (){
+  // Calling fetch function
   requestAirConditiong().then(function(result) {
+    // Calling caluculating function AFTER API has resolved promise
     getAverageCubicWeight(result)
-
   });
 })()
